16c006a1b29754252ea276e12e202d3214d81c19
r1sotto <r1sotto@github.com>
2022-04-21 10:52:30 +0300

    subarray sum equals k

---
+++ Leetcode/arrays/subarraySumEqualsK.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/subarray-sum-equals-k/
+
+def subarraySum(nums, k):
+    res = 0
+    curSum = 0
+    prefixSums = {0 : 1}
+
+    for n in nums:
+        curSum += n
+        diff = curSum - k
+        res += prefixSums.get(diff, 0)
+        prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
+
+    return res
