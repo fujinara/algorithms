f035229fd3d9e8143fa19d40643e09e5d359115c
r1sotto <r1sotto@github.com>
2022-05-05 01:02:12 +0500

    Jump Game

--- Leetcode/dp/jumpGame.py
+++ Leetcode/dp/jumpGame.py
@@ -1,4 +1,11 @@
 # https://leetcode.com/problems/jump-game/
 
 def canJump(nums: List[int]) -> bool:
-    pass
+    n = len(nums)
+    i, maxInd = 0, 0
+    while i <= maxInd:
+        if maxInd >= n - 1:
+            return True
+        maxInd = max(maxInd, i + nums[i])
+        i += 1
+    return False
