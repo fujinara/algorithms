59f11ab7d2806fc0039c8c90e3d245bdb7ac723d
r1sotto <r1sotto@github.com>
2022-06-24 10:04:37 +0500

    move zeros to the end of array

--- Random/moveZeros.py
+++ Random/moveZeros.py
@@ -1,14 +1,10 @@
 def moveZeros(arr):
     i, j = 0, 0
-    n = len(arr)
-    while j < n:
+    while j < len(arr):
         if arr[j] != 0:
-            arr[i] = arr[j]
+            arr[i], arr[j] = arr[j], arr[i]
             i += 1
             j += 1
         else:
             j += 1
-    while i < n:
-        arr[i] = 0
-        i += 1
     return arr
